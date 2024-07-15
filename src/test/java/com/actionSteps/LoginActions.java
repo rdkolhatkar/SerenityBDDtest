2133bfeb868faf185a846677631c18bd9e710157
Ratnakar <ratnakarkolhatkar@gmail.com>
2024-07-15 22:36:23 +0530

    Added Enum for security

--- src/test/java/com/actionSteps/LoginActions.java
+++ src/test/java/com/actionSteps/LoginActions.java
@@ -1,13 +1,15 @@
 package com.actionSteps;
 
+import com.userAuthentication.Users;
+import net.serenitybdd.annotations.Step;
 import net.serenitybdd.core.steps.UIInteractionSteps;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class LoginActions extends UIInteractionSteps {
 
+    @Step("Login as a Standard user")
     public void asAStandardUser(){
-
         /*
         // driver Methods from WebDriver
         // Login with Standard user
@@ -18,7 +20,6 @@ public class LoginActions extends UIInteractionSteps {
         // Validation by checking the title of product catalog
         assertThat(driver.findElement(By.cssSelector(".title")).getText()).isEqualToIgnoringCase("products");
         */
-
         // Serenity Methods from UIInteractionSteps
         // Login with Standard user
         openUrl("https://www.saucedemo.com");
@@ -27,7 +28,30 @@ public class LoginActions extends UIInteractionSteps {
         find("[data-test='login-button']").click();
         // Validation by checking the title of product catalog
         assertThat(find(".title").getText()).isEqualToIgnoringCase("products");
-
     }
+
+    @Step("Login as {0}")
+    public void usingCredentials(String username, String password){
+        openUrl("https://www.saucedemo.com");
+        find("[data-test='username']").sendKeys(username);
+        find("[data-test='password']").sendKeys(password);
+        find("[data-test='login-button']").click();
+        // Validation by checking the title of product catalog
+        assertThat(find(".title").getText()).isEqualToIgnoringCase("products");
+    }
+
+    @Step("Login as {0}")
+    public void as(Users users){
+        openUrl("https://www.saucedemo.com");
+        find("[data-test='username']").sendKeys(users.getUsername());
+        find("[data-test='password']").sendKeys(users.getPassword());
+        find("[data-test='login-button']").click();
+        // Validation by checking the title of product catalog
+        assertThat(find(".title").getText()).isEqualToIgnoringCase("products");
+    }
+
+
+
+
 
 }
--- src/test/java/com/userAuthentication/UserLoginActions.java
+++ src/test/java/com/userAuthentication/UserLoginActions.java
@@ -4,12 +4,14 @@ import com.actionSteps.LoginActions;
 import com.inventory.InventoryPage;
 import net.serenitybdd.annotations.Managed;
 import net.serenitybdd.annotations.Steps;
+import net.serenitybdd.core.Serenity;
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
+import static com.userAuthentication.Users.STANDARD_USER;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(SerenityRunner.class)
@@ -27,9 +29,16 @@ public class UserLoginActions {
 
         // Serenity Methods from UIInteractionSteps
         // Login with Standard user
-        login.asAStandardUser();
+        //login.asAStandardUser();
+        //login.usingCredentials("standard_user", "secret_sauce");
+        login.as(STANDARD_USER); // Created Enum for Security Purpose with Users Class
         // Validation by checking the title of product catalog
-        assertThat(inventoryPage.getHeading()).isEqualToIgnoringCase("products");
+        //assertThat(inventoryPage.getHeading()).isEqualToIgnoringCase("products");
+
+        // in-built serenity method with lambada expression for Asserting
+        Serenity.reportThat("The inventory page should be displayed with correct title",
+                () -> assertThat(inventoryPage.getHeading()).isEqualToIgnoringCase("products")
+        );
 
 
     }
