3df3ac74c582020133bbfba1c602a02dc38ca16a
Ratnakar <ratnakarkolhatkar@gmail.com>
2024-09-23 13:22:13 +0530

    Extract price of items from cart

--- src/test/java/com/addToCart/CartItems.java
+++ src/test/java/com/addToCart/CartItems.java
@@ -6,6 +6,6 @@ Record is a Java class that's used to represent data records, such as those retu
 It only gives us the read-only access for those records.
 There is no need to define the Getters and Setters java methods for this class
  */
-public record CartItems(String title, String description, String price) {
+public record CartItems(String title, String description, Double price) {
 
 }
--- src/test/java/com/addToCart/CartPageObject.java
+++ src/test/java/com/addToCart/CartPageObject.java
@@ -2,13 +2,45 @@ package com.addToCart;
 
 import net.serenitybdd.annotations.DefaultUrl;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // @DefaultUrl annotation in serenity will specify the URL that a page object should use when it's run in isolation.
 @DefaultUrl("https://www.saucedemo.com/cart.html")
 public class CartPageObject extends PageObject {
+
+//    @FindBy(id = "checkout")
+    @FindBy(css = "[data-test=checkout]")
+//    WebElement checkOutButton; // WebElement is a Selenium class
+    WebElementFacade checkOutButton; // This is a serenity class which gives us more flexibility on interacting with web elements.
+    @FindBy(css = ".title")
+    WebElementFacade title;
+
+    @FindBy(className = "cart_item")
+    List<WebElementFacade> cartItems;
+    public void checkout(){
+        checkOutButton.click();
+    }
+
+    public String getTitleText(){
+        return title.getText();
+    }
     public List<CartItems> items() {
+        List<CartItems> cartItemsList = new ArrayList<>();
+        for(WebElementFacade cartItemElement : cartItems){
+            String name = cartItemElement.findBy(".inventory_item_name").getText();
+            String description = cartItemElement.findBy(".inventory_item_desc").getText();
+            Double price = priceFrom(cartItemElement.findBy(".inventory_item_price").getText());
+            cartItemsList.add(new CartItems(name, description, price));
+        }
         return null;
+    }
+
+    private Double priceFrom(String textValue) {
+        return Double.parseDouble(textValue.replace("$", " "));
     }
 }
