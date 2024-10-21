1a424f9656c9087e9cee4554e944ddb06e89a06d
Ratnakar <ratnakarkolhatkar@gmail.com>
2024-10-21 17:00:27 +0530

    Added runner configuration test

--- src/main/java/com/web/test/runner/WebTestRunner.java
+++ src/main/java/com/web/test/runner/WebTestRunner.java
@@ -1,4 +1,14 @@
 package com.web.test.runner;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/main/resources/feature/WebTest.feature"},
+        tags = "@Test",
+        glue = {"com.web.test.stepDefinitions"}
+)
 public class WebTestRunner {
 }
