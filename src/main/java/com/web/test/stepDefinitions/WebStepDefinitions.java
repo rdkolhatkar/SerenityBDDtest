a5b2f18f48e7457b746092f6b1c89aef3e741680
Ratnakar <ratnakarkolhatkar@gmail.com>
2024-11-02 19:26:42 +0530

    Modified Step Definitions

--- src/main/java/com/web/test/stepDefinitions/WebStepDefinitions.java
+++ src/main/java/com/web/test/stepDefinitions/WebStepDefinitions.java
@@ -1,19 +1,35 @@
 package com.web.test.stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class WebStepDefinitions {
     @Given("{string} as {string}")
-    public void as(String key, String Value) {
-        
+    public void as(String arg0, String arg1) {
     }
+    @And("The {string} as {string}")
+    public void theAs(String arg0, String arg1) {
 
+    }
+    @And("The Mobile {string} as {string}")
+    public void theMobileAs(String arg0, String arg1) {
+    }
+    @And("The Student's date of birth {string} as {string}")
+    public void theStudentSDateOfBirthAs(String arg0, String arg1) {
+    }
+    @And("The Elective {string} as {string}")
+    public void theElectiveAs(String arg0, String arg1) {
+    }
+    @And("The Current Address {string} as {string}")
+    public void theCurrentAddressAs(String arg0, String arg1) {
+
+    }
     @When("Gender, Hobby, State and City are selected successfully")
     public void genderHobbyStateAndCityAreSelectedSuccessfully() {
-    }
 
+    }
     @Then("Click on Submit button")
     public void clickOnSubmitButton() {
     }
--- src/main/resources/feature/WebTest.feature
+++ src/main/resources/feature/WebTest.feature
@@ -1,13 +1,15 @@
 Feature: Validate Student Registration Form
+
   @Test
   Scenario Outline: Verify All Web Elements and UI Components of Student Registration Form
     Given "Student_Name" as "<name>"
-    And "Student_Email" as "<email>"
-    And "Student_Phone" as "<phone>"
-    And "Student_Date_Of_Birth" as "<DOB>"
-    And "Student_Subjects" as "<subjects>"
-    And "Student_Address" as "<currentAddress>"
+    And The "Student_Email" as "<email>"
+    And The Mobile "Student_Phone" as "<phone>"
+    And The Student's date of birth "Student_Date_Of_Birth" as "<DOB>"
+    And The Elective "Student_Subjects" as "<subjects>"
+    And The Current Address "Student_Address" as "<currentAddress>"
     When Gender, Hobby, State and City are selected successfully
     Then Click on Submit button
     Examples:
-      | name | email | phone | DOB | subjects | currentAddress |
+      | name    | email             | phone      | DOB        | subjects              | currentAddress                     |
+      | Sanjeev | sanjeev@gmail.com | 2432121091 | 14-09-1999 | Environmental Science | Street No. 12, SG. Apartment, Pune |
