6fd1ae4a5d05fa66a42e4d08b71a755cf5f3cefe
Ratnakar <ratnakarkolhatkar@gmail.com>
2024-11-04 10:43:53 +0530

    API Step def

--- src/bdd/java/com/api/test/stepDefinitions/ApiStepDefinitions.java
+++ src/bdd/java/com/api/test/stepDefinitions/ApiStepDefinitions.java
@@ -1,9 +1,13 @@
 package com.api.test.stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 
 public class ApiStepDefinitions {
     @Given("Attribute {string} as {string}")
     public void attributeAs(String arg0, String arg1) {
+    }
+    @And("The Attribute {string} as {string}")
+    public void theAttributeAs(String arg0, String arg1) {
     }
 }
