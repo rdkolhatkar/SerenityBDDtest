65aa796f67efd1413a20a953237f7e5269db7e3c
Ratnakar <ratnakarkolhatkar@gmail.com>
2024-11-16 21:29:16 +0530

    Student Application form

--- src/main/java/com/web/test/runner/WebTestRunner.java
+++ src/main/java/com/web/test/runner/WebTestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/main/resources/feature/WebTest.feature"},
+        features = {"src/main/resources/feature/StudentApplicationForm.feature"},
         tags = "@Test",
         glue = {"com.web.test.stepDefinitions"}
 )
