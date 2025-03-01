991be0231a6f83da4da8c6b519e7080acfcf283b
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-03-01 17:58:51 +0530

    CalendarDate Selector

--- src/test/java/com/timeouts/DynamicallyLoadingPage.java
+++ src/test/java/com/timeouts/DynamicallyLoadingPage.java
@@ -1,11 +1,26 @@
 package com.timeouts;
 
 import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.findby.By;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
 
 public class DynamicallyLoadingPage extends PageObject {
 
     public void start(){
         $("#start button").click();
+
+        // This is a Inbuilt serenity function used for waiting for an element by using expected conditions like in our case we have a loading bar.
+        // Our code will wait till that loading bar disappears from UI
+        //waitFor(ExpectedConditions.invisibilityOfElementLocated(By.id("loading")));
+
+        /*
+        This ExpectedConditions.visibilityOfElementLocated method from waitFor() will be holding our code execution till finish element is visible.
+         */
+        withTimeoutOf(Duration.ofSeconds(10));
+        waitFor(ExpectedConditions.visibilityOfElementLocated(By.id("finish")));
     }
     public String result(){
         return $("#finish").getText();
--- src/test/java/com/timeouts/SerenityTimeoutsWebPageLoading.java
+++ src/test/java/com/timeouts/SerenityTimeoutsWebPageLoading.java
@@ -5,6 +5,7 @@ import net.serenitybdd.annotations.Managed;
 import net.serenitybdd.core.Serenity;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,7 +14,10 @@ public class SerenityTimeoutsWebPageLoading {
 
     @Managed(driver = "chrome") // This line of code is to specify type of driver, No need to add driver.exe file separately. Serenity provides inbuilt driver settings
     WebDriver driver;
+    public WebDriver driverOne = new ChromeDriver();
     String webHostUrl = Serenity.environmentVariables().getProperty("app.base.url");
+    String endpoint = Serenity.environmentVariables().getProperty("dynamic_loading_hidden");
+    String dynamicLoadingHidden = webHostUrl+endpoint;
     DynamicallyLoadingPage dynamicallyLoadingPage;
     @Test
     public void waitForAnElementToAppear(){
@@ -22,6 +26,13 @@ public class SerenityTimeoutsWebPageLoading {
 
         assertThat(dynamicallyLoadingPage.result()).isEqualTo("Hello World!");
 
+    }
+    @Test
+    public void waitForAnHiddenElementToAppear(){
+        driverOne.get(dynamicLoadingHidden);
+        dynamicallyLoadingPage.start();
+
+        assertThat(dynamicallyLoadingPage.result()).isEqualTo("Hello World!");
     }
 
 
