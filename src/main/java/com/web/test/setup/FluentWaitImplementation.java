42d4a5a75d86d7af56421d917ef38510584db3c4
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-05-11 17:52:33 +0530

    Fluent Wait Implementation

---
+++ src/main/java/com/web/test/setup/FluentWaitImplementation.java
@@ -0,0 +1,38 @@
+package com.web.test.setup;
+
+import net.thucydides.core.annotations.findby.By;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+public class FluentWaitImplementation {
+
+    @Test
+    public void explicitFluentWaitTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.cssSelector("[id='start'] button")).click();
+
+        // As FluentWait cannot understand the interface we have to explicitly mention the WebDriver interface in the Syntax of FluentWait
+        //Syntax of defining the FluentWait is as below
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(3)).ignoring(NoSuchElementException.class);
+
+        WebElement element = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver){
+                return driver.findElement(By.cssSelector("[id='finish'] h4"));
+            }
+        });
+
+        System.out.println(driver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed());
+
+    }
+
+}
