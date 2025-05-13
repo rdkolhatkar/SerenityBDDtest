f4bda170d9c181e0c0899ec907edcc3707ac4b7e
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-05-13 13:38:44 +0530

    Extracting PlaceID

--- src/bdd/java/com/api/test/setup/ApiSetup.java
+++ src/bdd/java/com/api/test/setup/ApiSetup.java
@@ -1,6 +1,8 @@
 package com.api.test.setup;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -21,15 +23,23 @@ public class ApiSetup {
         // Read JSON file content as a String
         String requestBody = readJsonFile(jsonFilePath);
 
-        given().log().all().queryParam("key","qaclick123")
+       String response =  given().log().all().queryParam("key","qaclick123")
                 .header("Content-Type", "application/json")
                 .body(requestBody).when().post("/maps/api/place/add/json")
                 // Validating the API Response body
                 .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
                 // Validation of Response Headers
-                .header("Server", "Apache/2.4.18 (Ubuntu)");
+                .header("Server", "Apache/2.4.18 (Ubuntu)").extract().response().asString();
+
+        System.out.println(response);
+        // Now we have to parse the extracted Json response to retrieve the placeID
+        JsonPath jsonPath = new JsonPath(response); //JsonPath method is used to Parse the Json
+        String placeIdValue = jsonPath.getString("place_id");
+        System.out.println("place_id = "+placeIdValue);
     }
 
     // Add Place and Update Place and then validate if new address is present in response or not
+
+
 
 }
