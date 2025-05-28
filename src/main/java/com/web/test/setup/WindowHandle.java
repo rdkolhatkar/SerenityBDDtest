5ac306970e8ffc7faf6df33254a83243c3bd0e40
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-05-28 16:21:46 +0530

    Handling driver context on child windows

--- src/main/java/com/web/test/setup/WindowHandle.java
+++ src/main/java/com/web/test/setup/WindowHandle.java
@@ -20,7 +20,10 @@ public class WindowHandle {
         Iterator<String> iterator = windows.iterator(); // With this method we can Iterate the Set returned by the getWindowHandles() method
         String parenrId = iterator.next(); // Now control is on parent window ID that is Zero Index. Now to select the child window we have perform iterator.next() one more time
         String childId = iterator.next();
-        driver.switchTo().window(childId);
+        driver.switchTo().window(childId);// Now control is on the child window
         System.out.println(driver.findElement(By.cssSelector(".im-para.red")).getText());
+        String emailId = driver.findElement(By.cssSelector(".im-para.red")).getText().split("at")[1].trim().split(" ")[0]; // at this moment driver context is present inside the child window, we have to switch back to the parent window
+        driver.switchTo().window(parenrId);// We brought back control to the parent window again
+        driver.findElement(By.id("username")).sendKeys(emailId);
     }
 }
