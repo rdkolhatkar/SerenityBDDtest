fc2caa166d4a9b7915a89143f663d001c5b63685
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-05-31 18:48:16 +0530

    Performing drag and drop action inside frame

--- src/main/java/com/web/test/setup/HandlingUiFrames.java
+++ src/main/java/com/web/test/setup/HandlingUiFrames.java
@@ -1,8 +1,11 @@
 package com.web.test.setup;
 
+import net.thucydides.core.annotations.findby.By;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class HandlingUiFrames {
 
@@ -12,6 +15,18 @@ public class HandlingUiFrames {
         driver.get("http://jqueryui.com/droppable/");
         // Frames are sub-windows or sections within a single browser window. Frames are the part of the website which is not an Integral part of Web Element but they are hosted in our website as a part of external service.
         // To handle frames in the selenium below syntax is used
-        driver.switchTo().frame(1);
+        System.out.println(driver.findElements(By.tagName("iframe")).size()); // Counting the number of iframes present in the WebPage
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[class='demo-frame']")));
+        driver.findElement(By.id("draggable")).click();
+
+        //Drag and Drop Concept in the UI Web page and IFrames
+        Actions uiActions = new Actions(driver);
+        WebElement source = driver.findElement(By.id("draggable"));
+        WebElement target = driver.findElement(By.id("droppable"));
+        uiActions.dragAndDrop(source, target).build().perform();
+
+        //Now we have to go out of the frame and come back to default web page. For this we use the below method
+        driver.switchTo().defaultContent();
+
     }
 }
