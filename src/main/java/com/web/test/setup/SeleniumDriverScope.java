c58e1437e1a1de59b0bbaf0d370da3fd58423118
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-06-02 14:10:14 +0530

    Driver Scope

---
+++ src/main/java/com/web/test/setup/SeleniumDriverScope.java
@@ -0,0 +1,27 @@
+package com.web.test.setup;
+
+
+import net.thucydides.core.annotations.findby.By;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDriverScope {
+
+    @Test
+    public void seleniumWebDriverScope(){
+        //Calculate the total number of count of Links present in the webpage
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://qaclickacademy.com/practice.php");
+        driver.findElements(By.tagName("a")).size();
+        System.out.println(driver.findElements(By.tagName("a")).size());
+        //Calculate the number of links present in the footer section of web page
+        // For this above requirement we have to create the driver sub sets or mini driver, having limited scope to footer only
+        WebElement footerDriver = driver.findElement(By.id("gf-BIG"));
+        footerDriver.findElements(By.tagName("a")).size();
+        System.out.println(footerDriver.findElements(By.tagName("a")).size());
+    }
+
+
+}
