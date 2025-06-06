d09ef9fcf511022d0f1a7632793fcbac38882fd8
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-06-06 12:08:40 +0530

    Limiting the WebDriver Scope

--- src/main/java/com/web/test/setup/SeleniumDriverScope.java
+++ src/main/java/com/web/test/setup/SeleniumDriverScope.java
@@ -18,9 +18,18 @@ public class SeleniumDriverScope {
         System.out.println(driver.findElements(By.tagName("a")).size());
         //Calculate the number of links present in the footer section of web page
         // For this above requirement we have to create the driver sub sets or mini driver, having limited scope to footer only
-        WebElement footerDriver = driver.findElement(By.id("gf-BIG"));
+        WebElement footerDriver = driver.findElement(By.id("gf-BIG")); // limiting webdriver scope
         footerDriver.findElements(By.tagName("a")).size();
         System.out.println(footerDriver.findElements(By.tagName("a")).size());
+        //Calculate the number of links on the first column of the footer
+        WebElement columnDriver = footerDriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
+        columnDriver.findElements(By.tagName("a")).size();
+        System.out.println(columnDriver.findElements(By.tagName("a")).size());
+        //Click on each link in the first column and check if page is opening when we click on links
+        for(int i=1; i<columnDriver.findElements(By.tagName("a")).size(); i++){
+            columnDriver.findElements(By.tagName("a")).get(i).click();
+        }
+
     }
 
 
