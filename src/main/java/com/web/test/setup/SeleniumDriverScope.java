f3ccc301ad66072c1edeb692dcb20b87e1008629
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-06-08 14:10:40 +0530

    initializing Keyboard events to invoking multiple links and grabbing the titles

--- src/main/java/com/web/test/setup/SeleniumDriverScope.java
+++ src/main/java/com/web/test/setup/SeleniumDriverScope.java
@@ -8,10 +8,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class SeleniumDriverScope {
 
     @Test
-    public void seleniumWebDriverScope(){
+    public void seleniumWebDriverScope() throws InterruptedException {
         //Calculate the total number of count of Links present in the webpage
         WebDriver driver = new ChromeDriver();
         driver.get("http://qaclickacademy.com/practice.php");
@@ -26,12 +29,20 @@ public class SeleniumDriverScope {
         WebElement columnDriver = footerDriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
         columnDriver.findElements(By.tagName("a")).size();
         System.out.println(columnDriver.findElements(By.tagName("a")).size());
-        //Click on each link in the first column and check if page is opening when we click on links
+        //Click on each link in the first column and check if page is opening when we click on links and also get the title of each page
         for(int i=1; i<columnDriver.findElements(By.tagName("a")).size(); i++){
             // When we click on each link it will navigate to new web page in the same tab, we have to make sure all links are opening in the new tab and our base browser tab remains the same to click on next link
             // To solve this problem we have to press Ctrl + link to open link in new tab, for that we will use below method
             String clickOnLinkNewTab = Keys.chord(Keys.CONTROL,Keys.ENTER);
             columnDriver.findElements(By.tagName("a")).get(i).sendKeys(clickOnLinkNewTab); //.sendKeys(clickOnLinkNewTab) this method is used for initializing Keyboard events
+            // As 4 links are opening in four new tabs we have to switch to every child window and get the tile by using WindowHandles()
+            Thread.sleep(5000L);
+            Set<String> childWindows = driver.getWindowHandles();
+            Iterator<String> iterator = childWindows.iterator();
+            while(iterator.hasNext()){
+                driver.switchTo().window(iterator.next());
+                System.out.println(driver.getTitle());
+            }
         }
 
     }
