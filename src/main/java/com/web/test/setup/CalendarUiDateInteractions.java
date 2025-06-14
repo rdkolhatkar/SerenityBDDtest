e2be4888938d9dcb82fe0d40279d7496974a93ba
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-06-14 14:51:30 +0530

    Calendar UI Assertion & Validation

--- src/main/java/com/web/test/setup/CalendarUiDateInteractions.java
+++ src/main/java/com/web/test/setup/CalendarUiDateInteractions.java
@@ -1,9 +1,13 @@
 package com.web.test.setup;
 
 import net.thucydides.core.annotations.findby.By;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class CalendarUiDateInteractions {
 
@@ -12,6 +16,7 @@ public class CalendarUiDateInteractions {
         String monthNumber = "6";
         String date = "15";
         String year = "2027";
+        String[] expectedDateList = {monthNumber, date, year};
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
@@ -22,6 +27,14 @@ public class CalendarUiDateInteractions {
         // In the calendar Months are displayed as January, February, March, .... , December. But in our case we are receiving input in the number format like Month number is 6 and we have to configure Month Number with Month name like 6 == June
         driver.findElements(By.cssSelector(".react-calendar__year-view__months__month")).get(Integer.parseInt(monthNumber)- 1).click();
         driver.findElement(By.xpath("//abbr[text()='"+date+"']")).click();
-
+//        driver.findElement(By.cssSelector(".react-date-picker__inputGroup")).getText();
+        // Here we cannot assert directly by getText() method because date format is divided into sub classes in the HTML code like 6/15/2027 - MM/DD/YYYY
+       List<WebElement> actualDateList =  driver.findElements(By.cssSelector(".react-date-picker__inputGroup__input"));
+       for(int i =0; i<actualDateList.size(); i++){
+           actualDateList.get(i).getAttribute("value");
+           System.out.println(actualDateList.get(i).getAttribute("value"));
+           Assert.assertEquals(actualDateList.get(i).getAttribute("value"), expectedDateList[i]);
+       }
+       driver.close();
     }
 }
