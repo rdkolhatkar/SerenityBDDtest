0d8f79160d797f1847b0f8df304ab07506138082
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-06-27 21:11:48 +0530

    SSL Certification Error

---
+++ src/main/java/com/web/test/setup/SslCertificationHandling.java
@@ -0,0 +1,30 @@
+package com.web.test.setup;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class SslCertificationHandling {
+    @Test
+    public void badSslCertificationErrorHandling(){
+        // To handle bad ssl certification error we have to use below code to bypass the ssl error and open the actual website
+        // For Google Chrome
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setAcceptInsecureCerts(true); // This code will allow us to accept the bad ssl warning and bypass the ssl error and open the actual website
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://expired.badssl.com/");
+        System.out.println(driver.getTitle());
+
+        // For Firefox browser
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setAcceptInsecureCerts(true);
+
+        // For Microsoft Edge browser
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.setAcceptInsecureCerts(true);
+
+    }
+}
