b12a11a1e63677fb5e897198c8a9d22c7c2a0edf
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-07-04 11:37:41 +0530

    Soft Assertions for validating the all broken inks

--- src/main/java/com/web/test/setup/HandlingBrokenLinks.java
+++ src/main/java/com/web/test/setup/HandlingBrokenLinks.java
@@ -1,15 +1,16 @@
 package com.web.test.setup;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.asserts.SoftAssert;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
+import java.util.List;
 
 public class HandlingBrokenLinks {
     @Test
@@ -27,11 +28,62 @@ public class HandlingBrokenLinks {
         connection.connect(); //.connect() method is used to invoke the route on the browser to establish the connection
         int httpResponseCode = connection.getResponseCode(); //.getResponseCode() method is used to check the http response of a route
         System.out.println(httpResponseCode);
-
     }
 
     @Test
-    public void handlingMultipleLinksToSearchBrokenLink(){
+    public void handlingMultipleLinksToSearchBrokenLink() throws IOException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice");
+        List<WebElement> allLinks = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
+        for(WebElement link : allLinks){
+            String url = link.getAttribute("href");
+            HttpURLConnection connection = (HttpURLConnection)new URL(url).openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.connect();
+            int httpResponseCode = connection.getResponseCode();
+            System.out.println(httpResponseCode);
+            if(httpResponseCode > 400){
+                System.out.println("The Link With Text "+link.getText()+" is broken with code "+httpResponseCode);
+                Assert.assertTrue(false);
+            }
+        }
+    }
 
+    @Test
+    public void validatingBrokenLinksWithHardAssertions() throws IOException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice");
+        List<WebElement> allLinks = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
+        for(WebElement link : allLinks){
+            String url = link.getAttribute("href");
+            HttpURLConnection connection = (HttpURLConnection)new URL(url).openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.connect();
+            int httpResponseCode = connection.getResponseCode();
+            System.out.println("Url : "+link.getText()+" HTTP Status Code: "+httpResponseCode);
+            // Hard Assertion -> In this test will fail immediately after it found any broken link or assertion conditions fail, test will no go further to check the remaining links
+            Assert.assertTrue("The Link With Text "+link.getText()+" is broken with code "+httpResponseCode,httpResponseCode < 400);
+        }
+    }
+
+    @Test
+    public void validatingBrokenLinksWithSoftAssertions() throws IOException {
+        WebDriver driver = new ChromeDriver();
+        SoftAssert softAssert = new SoftAssert();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice");
+        List<WebElement> allLinks = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
+        for(WebElement link : allLinks){
+            String url = link.getAttribute("href");
+            HttpURLConnection connection = (HttpURLConnection)new URL(url).openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.connect();
+            int httpResponseCode = connection.getResponseCode();
+            System.out.println("Url : "+link.getText()+" HTTP Status Code: "+httpResponseCode);
+            // Soft Assertions -> in this test will not fail immediately after finding the broken link but it will go on to check the remaining links as well and then after all links are checked it will fail at very last
+            softAssert.assertTrue(httpResponseCode < 400,"The Link With Text "+link.getText()+" is broken with code "+httpResponseCode);
+        }
+        // To continue our assertions for remaining links we have to use below code at the end of for loop
+        // .assertAll() will catch all the assertion reports for every link
+        softAssert.assertAll();
     }
 }
