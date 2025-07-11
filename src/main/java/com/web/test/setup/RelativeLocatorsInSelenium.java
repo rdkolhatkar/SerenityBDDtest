70c620020951e5fc008ff10b81c7fa2a2fa62806
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-07-11 17:59:51 +0530

    Relative locators in selenium 4 with implementation

--- src/main/java/com/web/test/setup/RelativeLocatorsInSelenium.java
+++ src/main/java/com/web/test/setup/RelativeLocatorsInSelenium.java
@@ -11,6 +11,7 @@ public class RelativeLocatorsInSelenium {
 
     @Test
     public void relativeLocatorsInSeleniumFour(){
+        // Relative locators are introduced in the selenium 4
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/angularpractice/");
         WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
@@ -18,6 +19,14 @@ public class RelativeLocatorsInSelenium {
         // To use relative locators in our project we have to manually import one static package called {import static org.openqa.selenium.support.locators.RelativeLocator.*;}
         String elementName = driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText();
         System.out.println(elementName);
+        WebElement dateOfBirth = driver.findElement(By.cssSelector("[for='dateofBirth']"));
+        driver.findElement(with(By.tagName("input")).below(dateOfBirth)).click();
+        // relative locators do not support flex html elements
+        WebElement iceCreamLabel = driver.findElement(By.xpath("//label[text()='Check me out if you Love IceCreams!']"));
+        driver.findElement(with(By.tagName("input")).toLeftOf(iceCreamLabel)).click();
+        WebElement radioBtn = driver.findElement(By.id("inlineRadio1"));
+        String radioBtnText = driver.findElement(with(By.tagName("label")).toRightOf(radioBtn)).getText();
+        System.out.println(radioBtnText);
         driver.close();
     }
 }
